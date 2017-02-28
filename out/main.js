7ca976290fe53c5decd67924bc5cdbdd99b440d2
PhoenixMo <1002375424@qq.com>
2017-02-28 13:35:36 +0800

    Finish Homework 3

--- out/main.js
+++ out/main.js
@@ -1,8 +1,4 @@
-var __extends = (this && this.__extends) || function (d, b) {
-    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
-    function __() { this.constructor = d; }
-    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-};
+var _this = this;
 window.onload = function () {
     var currentTarget; //鼠标点击时当前的对象
     var startTarget; //mouseDown时的对象
@@ -11,229 +7,89 @@ window.onload = function () {
     var movingPoint = new math.Point(0, 0);
     var canvas = document.getElementById("context");
     var context2d = canvas.getContext("2d");
-    var Bg = new DisplayObjectContainer();
+    var stage = new DisplayObjectContainer();
+    var Container = new DisplayObjectContainer();
     var myPhoto = new Bitmap("myPhoto.jpg");
     var myPhoto2 = new Bitmap("1.png");
-    Bg.addChild(myPhoto);
-    Bg.addChild(myPhoto2);
-    Bg.draw(context2d);
+    myPhoto.alpha = 0.5;
+    stage.addChild(Container);
+    Container.addChild(myPhoto);
+    Container.addChild(myPhoto2);
+    stage.draw(context2d);
+    stage.addEventListener(TouchEventsType.MOUSEDOWN, function () {
+        console.log("stage");
+    }, _this);
+    Container.addEventListener(TouchEventsType.MOUSEMOVE, function () {
+        // console.log("container");
+    }, _this);
+    myPhoto.addEventListener(TouchEventsType.MOUSEMOVE, function () {
+        if (currentTarget == startTarget) {
+            Container.x += (TouchEventService.stageX - movingPoint.x);
+            Container.y += (TouchEventService.stageY - movingPoint.y);
+            console.log("myP");
+        }
+    }, _this);
+    myPhoto2.addEventListener(TouchEventsType.CLICK, function () {
+        alert("OnClick!!!");
+        console.log("2");
+    }, _this);
+    window.onmousedown = function (e) {
+        var x = e.offsetX - 3;
+        var y = e.offsetY - 3;
+        TouchEventService.stageX = x;
+        TouchEventService.stageY = y;
+        startPoint.x = x;
+        startPoint.y = y;
+        movingPoint.x = x;
+        movingPoint.y = y;
+        TouchEventService.currentType = TouchEventsType.MOUSEDOWN;
+        currentTarget = stage.hitTest(x, y);
+        startTarget = currentTarget;
+        TouchEventService.getInstance().toDo();
+        //  console.log(stage.globalMatrix);
+        //console.log(currentTarget);
+        //TouchEventService.getInstance().clearList();
+        isMouseDown = true;
+        //console.log(TouchEventService.currentType);
+    };
+    window.onmouseup = function (e) {
+        var x = e.offsetX - 3;
+        var y = e.offsetY - 3;
+        TouchEventService.stageX = x;
+        TouchEventService.stageY = y;
+        var target = stage.hitTest(x, y);
+        //console.log(target);
+        if (target == currentTarget) {
+            TouchEventService.currentType = TouchEventsType.CLICK;
+        }
+        else {
+            TouchEventService.currentType = TouchEventsType.MOUSEUP;
+        }
+        TouchEventService.getInstance().toDo();
+        //TouchEventService.getInstance().clearList();
+        //console.log(TouchEventService.currentType);
+        currentTarget = null;
+        isMouseDown = false;
+    };
+    window.onmousemove = function (e) {
+        if (isMouseDown) {
+            var x = e.offsetX - 3;
+            var y = e.offsetY - 3;
+            TouchEventService.stageX = x;
+            TouchEventService.stageY = y;
+            TouchEventService.currentType = TouchEventsType.MOUSEMOVE;
+            console.log(TouchEventService.currentType);
+            currentTarget = stage.hitTest(x, y);
+            TouchEventService.getInstance().toDo();
+            movingPoint.x = x;
+            movingPoint.y = y;
+        }
+    };
     setInterval(function () {
         context2d.save();
         context2d.clearRect(0, 0, canvas.width, canvas.height);
-        Bg.draw(context2d);
+        stage.draw(context2d);
         context2d.restore();
     }, 60);
 };
-var DisplayObject = (function () {
-    function DisplayObject() {
-        this.x = 0;
-        this.y = 0;
-        this.width = 1;
-        this.height = 1;
-        this.scaleX = 1;
-        this.scaleY = 1;
-        this.rotation = 0;
-        this.alpha = 1;
-        this.globalAlpha = 1; //全局                             
-        this.parent = null;
-        this.touchEnabled = false;
-        this.listeners = [];
-        this.matrix = new math.Matrix();
-        this.globalMatrix = new math.Matrix();
-    }
-    //每个子类都要这么干，final
-    DisplayObject.prototype.draw = function (canvas) {
-        this.matrix.updateFromDisplayObject(this.x, this.y, this.scaleX, this.scaleY, this.rotation); //初始化矩阵
-        if (this.parent) {
-            this.globalAlpha = this.parent.globalAlpha * this.alpha;
-            this.globalMatrix = math.matrixAppendMatrix(this.matrix, this.parent.globalMatrix);
-        }
-        else {
-            this.globalAlpha = this.alpha;
-            this.globalMatrix = this.matrix;
-        }
-        canvas.globalAlpha = this.globalAlpha;
-        canvas.setTransform(this.globalMatrix.a, this.globalMatrix.b, this.globalMatrix.c, this.globalMatrix.d, this.globalMatrix.tx, this.globalMatrix.ty);
-        this.render(canvas);
-        //模板方法模式
-    };
-    DisplayObject.prototype.addEventListener = function (type, touchFunction, object, ifCapture, priority) {
-        var touchEvent = new TouchEvents(type, touchFunction, object, ifCapture, priority);
-        this.listeners.push(touchEvent);
-    };
-    return DisplayObject;
-}());
-var DisplayObjectContainer = (function (_super) {
-    __extends(DisplayObjectContainer, _super);
-    function DisplayObjectContainer() {
-        var _this = _super !== null && _super.apply(this, arguments) || this;
-        _this.list = [];
-        return _this;
-    }
-    DisplayObjectContainer.prototype.addChild = function (child) {
-        if (this.list.indexOf(child) == -1) {
-            this.list.push(child);
-            child.parent = this;
-        }
-    };
-    DisplayObjectContainer.prototype.removeChild = function (child) {
-        for (var _i = 0, _a = this.list; _i < _a.length; _i++) {
-            var element = _a[_i];
-            if (element == child) {
-                var index = this.list.indexOf(child);
-                this.list.splice(index);
-                return;
-            }
-        }
-    };
-    DisplayObjectContainer.prototype.removeall = function () {
-        this.list = [];
-    };
-    DisplayObjectContainer.prototype.render = function (canvas) {
-        for (var _i = 0, _a = this.list; _i < _a.length; _i++) {
-            var child = _a[_i];
-            child.draw(canvas);
-        }
-    };
-    DisplayObjectContainer.prototype.hitTest = function (x, y) {
-        // console.log(x);
-        // console.log(y);
-        var rect = new math.Rectangle();
-        rect.x = rect.y = 0;
-        rect.width = this.width;
-        rect.height = this.height;
-        var result = null;
-        if (rect.isPointInRectangle(x, y)) {
-            // for(var listener of this.listeners){
-            //     if(listener.type == TouchEventService.currentType && listener.capture){
-            //         //TouchEventService.getInstance().addPerformer(listener.func());   //捕获
-            //         //listener.func();
-            //         // TouchEventService.getInstance().addPerformer(this);
-            //     }
-            // }
-            result = this;
-            TouchEventService.getInstance().addPerformer(this); //从父到子把相关对象存入数组
-            for (var i = this.list.length - 1; i >= 0; i--) {
-                var child = this.list[i];
-                var point = new math.Point(x, y);
-                var invertChildenLocalMatirx = math.invertMatrix(child.matrix);
-                var pointBasedOnChild = math.pointAppendMatrix(point, invertChildenLocalMatirx);
-                var hitTestResult = child.hitTest(pointBasedOnChild.x, pointBasedOnChild.y);
-                //console.log(hitTestResult);
-                if (hitTestResult) {
-                    result = hitTestResult;
-                    break;
-                }
-            }
-            // for(var listener of this.listeners){
-            //     if(listener.type == TouchEventService.currentType){
-            //         //listener.func();
-            //         TouchEventService.getInstance().addPerformer(this);
-            //     }
-            // }
-            //TouchEventService.getInstance().addPerformer(this);
-            return result;
-        }
-        return null;
-    };
-    return DisplayObjectContainer;
-}(DisplayObject));
-var TextField = (function (_super) {
-    __extends(TextField, _super);
-    function TextField() {
-        var _this = _super !== null && _super.apply(this, arguments) || this;
-        _this.text = "";
-        _this.color = "";
-        _this.fontSize = 10;
-        _this.fontName = "";
-        return _this;
-    }
-    TextField.prototype.render = function (canvas) {
-        canvas.fillStyle = this.color;
-        canvas.globalAlpha = this.alpha;
-        canvas.font = this.fontSize.toString() + "px " + this.fontName.toString();
-        canvas.fillText(this.text, this.x, this.y + this.fontSize);
-    };
-    TextField.prototype.hitTest = function (x, y) {
-        // console.log("text" + x);
-        // console.log("text" + y);
-        var rect = new math.Rectangle();
-        rect.x = rect.y = 0;
-        rect.width = this.fontSize * this.text.length;
-        rect.height = this.fontSize;
-        if (rect.isPointInRectangle(x, y)) {
-            // for(var listener of this.listeners){
-            //     if(listener.type == TouchEventService.currentType){
-            //         listener.func();
-            //     }
-            // }
-            TouchEventService.getInstance().addPerformer(this);
-            return this;
-        }
-        else {
-            return null;
-        }
-    };
-    return TextField;
-}(DisplayObject));
-var Bitmap = (function (_super) {
-    __extends(Bitmap, _super);
-    function Bitmap(id) {
-        var _this = _super.call(this) || this;
-        _this.img = null;
-        _this.isLoaded = false;
-        _this._src = "";
-        _this.img = new Image();
-        _this.img.src = id;
-        _this.img.onload = function () {
-            _this.width = _this.img.width;
-            _this.height = _this.img.height;
-        };
-        return _this;
-    }
-    Object.defineProperty(Bitmap.prototype, "src", {
-        set: function (value) {
-            this._src = value;
-            this.isLoaded = false;
-        },
-        enumerable: true,
-        configurable: true
-    });
-    Bitmap.prototype.render = function (canvas) {
-        var _this = this;
-        canvas.globalAlpha = this.alpha;
-        if (this.isLoaded) {
-            canvas.drawImage(this.img, this.x, this.y, this.img.width * this.scaleX, this.img.height * this.scaleY);
-        }
-        else {
-            this.img.src = this._src;
-            //   console.log(this.img.src);
-            this.img.onload = function () {
-                canvas.drawImage(_this.img, _this.x, _this.y, _this.img.width * _this.scaleX, _this.img.height * _this.scaleY);
-                _this.isLoaded = true;
-            };
-        }
-    };
-    Bitmap.prototype.hitTest = function (x, y) {
-        // console.log("image" + x);
-        // console.log("image" + y);
-        var rect = new math.Rectangle();
-        rect.x = rect.y = 0;
-        rect.width = this.img.width;
-        rect.height = this.img.height;
-        if (rect.isPointInRectangle(x, y)) {
-            // for(var listener of this.listeners){
-            //     if(listener.type == type){
-            //         listener.func();
-            //     }
-            // }
-            TouchEventService.getInstance().addPerformer(this);
-            return this;
-        }
-        else {
-            return null;
-        }
-    };
-    return Bitmap;
-}(DisplayObject));
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -10,26 +10,118 @@ window.onload = () => {
 
     var canvas = document.getElementById("context") as HTMLCanvasElement;
     var context2d = canvas.getContext("2d");
-    var Bg = new DisplayObjectContainer();
+    var stage = new DisplayObjectContainer();
 
-
+    var Container = new DisplayObjectContainer();
 
 
 
 
     var myPhoto = new Bitmap("myPhoto.jpg");
     var myPhoto2 = new Bitmap("1.png");
-   
   
-    Bg.addChild(myPhoto);
-    Bg.addChild(myPhoto2);
+    myPhoto.alpha=0.5;
     
-    Bg.draw(context2d);
+
+    stage.addChild(Container);
+    Container.addChild(myPhoto);
+    Container.addChild(myPhoto2);
+    
+    stage.draw(context2d);
+
+
+    stage.addEventListener(TouchEventsType.MOUSEDOWN,()=>{
+        console.log("stage");
+    },this)
+
+    Container.addEventListener(TouchEventsType.MOUSEMOVE,()=>{
+        // console.log("container");
+    },this)
+
+    myPhoto.addEventListener(TouchEventsType.MOUSEMOVE,()=>{
+        if(currentTarget == startTarget){
+        Container.x += (TouchEventService.stageX - movingPoint.x);
+        Container.y += (TouchEventService.stageY - movingPoint.y);
+        console.log("myP");
+        }
+    },this);
+
+    myPhoto2.addEventListener(TouchEventsType.CLICK,()=>{
+        alert("OnClick!!!");
+        console.log("2");
+    },this);
+
+
+
+
+    window.onmousedown = (e) =>{
+        let x = e.offsetX - 3;
+        let y = e.offsetY - 3;
+        TouchEventService.stageX = x;
+        TouchEventService.stageY = y;
+        startPoint.x = x;
+        startPoint.y = y;
+        movingPoint.x = x;
+        movingPoint.y = y;
+        TouchEventService.currentType = TouchEventsType.MOUSEDOWN;
+        currentTarget = stage.hitTest(x,y);
+        startTarget = currentTarget;
+        TouchEventService.getInstance().toDo();
+      //  console.log(stage.globalMatrix);
+        //console.log(currentTarget);
+        //TouchEventService.getInstance().clearList();
+        isMouseDown = true;
+        //console.log(TouchEventService.currentType);
+    }
+
+    window.onmouseup = (e) =>{
+        let x = e.offsetX - 3;
+        let y = e.offsetY - 3;
+        TouchEventService.stageX = x;
+        TouchEventService.stageY = y;
+        var target = stage.hitTest(x,y);
+        //console.log(target);
+        if(target == currentTarget){
+            TouchEventService.currentType = TouchEventsType.CLICK;
+        }
+        else{
+            TouchEventService.currentType = TouchEventsType.MOUSEUP
+        }
+        TouchEventService.getInstance().toDo();
+        //TouchEventService.getInstance().clearList();
+        //console.log(TouchEventService.currentType);
+        currentTarget = null;
+        isMouseDown = false;
+    }
+
+    window.onmousemove = (e) =>{
+        if(isMouseDown){
+            let x = e.offsetX - 3;
+            let y = e.offsetY - 3;
+            TouchEventService.stageX = x;
+            TouchEventService.stageY = y;
+            TouchEventService.currentType = TouchEventsType.MOUSEMOVE;
+            console.log(TouchEventService.currentType);
+            currentTarget = stage.hitTest(x,y);
+            TouchEventService.getInstance().toDo();
+            movingPoint.x = x;
+            movingPoint.y = y;
+            //TouchEventService.getInstance().clearList();
+
+        }
+    }
+
+
+
+
+
+
+
 
      setInterval(() => {
          context2d.save();
          context2d.clearRect(0, 0, canvas.width, canvas.height);
-          Bg.draw( context2d);
+          stage.draw( context2d);
           context2d.restore();
     }, 60)
 
@@ -41,239 +133,3 @@ window.onload = () => {
 
 
 
-interface Drawable {
-    draw(canvas: CanvasRenderingContext2D);
-}
-
-abstract class DisplayObject implements Drawable {
-
-
-
-    x = 0;
-    y = 0;
-    width = 1;
-    height = 1;
-    scaleX : number = 1;
-    scaleY : number = 1;
-    rotation : number = 0;
-
-    matrix : math.Matrix ;
-    globalMatrix : math.Matrix ;
-
-    alpha  = 1;
-    globalAlpha  = 1;//全局                             
-    parent :  DisplayObjectContainer =null;
-
-    touchEnabled:boolean =false;
-    listeners : TouchEvents[] = [];
-
-
-     constructor(){
-        this.matrix = new math.Matrix();
-        this.globalMatrix = new math.Matrix();
-     }
-
-    
-
-
-
-    //每个子类都要这么干，final
-    draw(canvas: CanvasRenderingContext2D) {
-        this.matrix.updateFromDisplayObject(this.x,this.y, this.scaleX, this.scaleY, this.rotation);//初始化矩阵
-        
-        if(this.parent){
-            this.globalAlpha = this.parent.globalAlpha * this.alpha;
-            this.globalMatrix = math.matrixAppendMatrix(this.matrix, this.parent.globalMatrix);
-           
-      
-        }else{
-            this.globalAlpha = this.alpha;
-            this.globalMatrix = this.matrix;
-        }
-        canvas.globalAlpha = this.globalAlpha;
-        canvas.setTransform(this.globalMatrix.a, this.globalMatrix.b, this.globalMatrix.c, this.globalMatrix.d, this.globalMatrix.tx, this.globalMatrix.ty);
-        this.render(canvas);
-
-        //模板方法模式
-    }
-   abstract    render(context2D: CanvasRenderingContext2D)
-
-     addEventListener(type : TouchEventsType,touchFunction : Function,object : any,ifCapture? : boolean,priority?: number){
-        var touchEvent = new TouchEvents(type,touchFunction,object,ifCapture,priority);
-        this.listeners.push(touchEvent);
-    }
-abstract hitTest(x : number,y : number):DisplayObject 
-}
-
-
-
-class DisplayObjectContainer extends DisplayObject  {
-    list: DisplayObject[] = [];
-
-    
-
-    addChild(child: DisplayObject) {
-        if (this.list.indexOf(child) == -1) {
-            this.list.push(child);
-            child.parent = this;
-        }
-    }
-    removeChild(child: DisplayObject) {
-        for (var element of this.list) {
-            if (element == child) {
-                var index = this.list.indexOf(child);
-                this.list.splice(index);
-                return;
-            }
-        }
-    }
-
-     removeall() {
-
-        this.list = [];
-
-    }
-
-    render(canvas: CanvasRenderingContext2D) {
-        for (var child of this.list) {
-            child.draw(canvas);
-        }
-    }
-   hitTest(x : number,y: number) : DisplayObject{
-        // console.log(x);
-        // console.log(y);
-        var rect = new math.Rectangle();
-        rect.x = rect.y = 0;
-        rect.width = this.width;
-        rect.height = this.height;
-        var result = null;
-        if(rect.isPointInRectangle(x,y)){
-            // for(var listener of this.listeners){
-            //     if(listener.type == TouchEventService.currentType && listener.capture){
-            //         //TouchEventService.getInstance().addPerformer(listener.func());   //捕获
-            //         //listener.func();
-            //         // TouchEventService.getInstance().addPerformer(this);
-            //     }
-            // }
-            result = this;
-            TouchEventService.getInstance().addPerformer(this);//从父到子把相关对象存入数组
-
-
-            for(let i = this.list.length - 1;i >= 0;i--){
-                var child = this.list[i];
-                var point = new math.Point(x,y);
-                var invertChildenLocalMatirx = math.invertMatrix(child.matrix);
-                var pointBasedOnChild = math.pointAppendMatrix(point,invertChildenLocalMatirx);
-                var hitTestResult = child.hitTest(pointBasedOnChild.x,pointBasedOnChild.y);
-                //console.log(hitTestResult);
-                if(hitTestResult){
-                    result = hitTestResult;
-                    break;
-                }
-            }
-            // for(var listener of this.listeners){
-            //     if(listener.type == TouchEventService.currentType){
-            //         //listener.func();
-            //         TouchEventService.getInstance().addPerformer(this);
-            //     }
-            // }
-            //TouchEventService.getInstance().addPerformer(this);
-            return result;
-        }
-
-        return null;
-    }
-
-
-}
-
-
-
-class TextField extends DisplayObject {
-    text = "";
-    color = "";
-    fontSize = 10;
-    fontName = "";
-    render(canvas: CanvasRenderingContext2D) {
-        canvas.fillStyle = this.color;
-        canvas.globalAlpha = this.alpha;
-        canvas.font = this.fontSize.toString() + "px " + this.fontName.toString();
-        canvas.fillText(this.text, this.x, this.y + this.fontSize);
-    }
-     hitTest(x : number,y :number){
-        // console.log("text" + x);
-        // console.log("text" + y);
-        var rect = new math.Rectangle();
-        rect.x = rect.y = 0;
-        rect.width = this.fontSize * this.text.length;
-        rect.height = this.fontSize;
-        if(rect.isPointInRectangle(x,y)){
-            // for(var listener of this.listeners){
-            //     if(listener.type == TouchEventService.currentType){
-            //         listener.func();
-            //     }
-            // }
-            TouchEventService.getInstance().addPerformer(this);
-            return this;
-        }
-        else{
-            return null;
-        }
-    }
-}
-
-class Bitmap extends DisplayObject {
-    private img: HTMLImageElement = null;
-    private isLoaded = false;
-    constructor(id : string) {
-       super();
-      
-        this.img = new Image();
-        this.img.src = id;
-        this.img.onload = () =>{
-            this.width = this.img.width;
-            this.height = this.img.height;
-        }
-    }
-    private _src = "";
-    set src(value: string) {
-        this._src = value;
-        this.isLoaded = false;
-    }
-
-    render(canvas: CanvasRenderingContext2D) {
-        canvas.globalAlpha = this.alpha;
-        if (this.isLoaded) {
-            canvas.drawImage(this.img, this.x, this.y, this.img.width * this.scaleX, this.img.height * this.scaleY);
-        }
-        else {
-           
-            this.img.src = this._src;
-         //   console.log(this.img.src);
-            this.img.onload = () => {
-                canvas.drawImage(this.img, this.x, this.y, this.img.width * this.scaleX, this.img.height * this.scaleY);
-                this.isLoaded = true;
-            }
-        }
-    }
-    hitTest(x : number,y :number){
-        // console.log("image" + x);
-        // console.log("image" + y);
-        var rect = new math.Rectangle();
-        rect.x = rect.y = 0;
-        rect.width = this.img.width;
-        rect.height = this.img.height;
-        if(rect.isPointInRectangle(x,y)){
-            // for(var listener of this.listeners){
-            //     if(listener.type == type){
-            //         listener.func();
-            //     }
-            // }
-            TouchEventService.getInstance().addPerformer(this);
-            return this;
-        }
-        else{
-            return null;
-        }
-    }
-}
